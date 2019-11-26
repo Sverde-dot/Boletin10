86bb3acdb3a36c81bd733466bada9add3b09d521
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2019-11-26 08:39:01 +0100

    Add files via upload

--- pkg2/Boletin102.java
+++ pkg2/Boletin102.java
@@ -1,6 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package boletin10.pkg2;
+
+/**
+ *
+ * @author sverdecadilla
+ */
 public class Boletin102 {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        Clase2 obx = new Clase2();
+        obx.jugador();
         
     }
     
--- pkg2/Clase2.java
+++ pkg2/Clase2.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boletin10.pkg2;
 
-/**
- *
- * @author Yo
- */
+import javax.swing.JOptionPane;
+
+
 public class Clase2 {
     
+        private int vidas;
+        private int Ad;
+        private int partida=0;
+        private int resta;
+        
+    public void Juego(){
+    }
+    
+    public void maquina(){
+        Ad=(int)(Math.random()*50)+1;
+    }
+        public void jugador(){
+        int numIntro,vidas2;
+        vidas=Integer.parseInt(JOptionPane.showInputDialog(null,"Introduzca el numero de intentos."));
+        for(vidas2=1;vidas2<=vidas;vidas2++){
+        numIntro=Integer.parseInt(JOptionPane.showInputDialog(null,"Jugador.\nIntroduzca un numero."));
+        while(numIntro<1||numIntro>50){
+        numIntro=Integer.parseInt(JOptionPane.showInputDialog(null, "Jugador.\nIntroduzca una cifra entre 1 y 50."));
+        }
+        resta=Math.abs(Ad-numIntro);
+        
+        if(resta>=20){
+            System.out.println("Estas muy lejos");
+            System.out.println("El numero es"+Ad);
+        }
+        else if(resta<20 && resta>=10){
+            System.out.println("No tan lejos");
+        }
+        else if(resta<10 && resta>=5){
+            System.out.println("Estas acercandote");
+        }
+        else if(resta<5)
+            System.out.println("Estas muy cerca");
+        else{
+            System.out.println("Acertaste");
+        }
+        if(vidas2>=vidas){
+             JOptionPane.showMessageDialog(null, "Perdiste...");
+        }
+          
+        }
+        }
 }
