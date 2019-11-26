1e57823f030a638fdae861795f43bb60797a93c5
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2019-11-26 08:36:30 +0100

    Update Boletin101.java

--- pkg1/Boletin101.java
+++ pkg1/Boletin101.java
@@ -1,19 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin10.pkg1;
-
-/**
- *
- * @author Yo
- */
 public class Boletin101 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Clase1 obx=new Clase1();
         
